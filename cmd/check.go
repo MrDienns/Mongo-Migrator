84ed77a6014d3d644c7abfe3a4783f2782e399ec
Dennis van der Veeke <MrDienns@dyescape.com>
2019-09-01 16:19:09 +0200

    now also reading environment variables for check cmd

--- cmd/check.go
+++ cmd/check.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -17,6 +18,7 @@ var (
 )
 
 func init() {
+	viper.AutomaticEnv()
 	checkCmd.Flags().Bool("delete", false, "delete files not part of this package")
 	checkCmd.Flags().String("host", "localhost", "the database host")
 	checkCmd.Flags().Int("port", 27017, "the database port")
