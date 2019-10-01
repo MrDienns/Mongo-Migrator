cb9c6981f13d1972238d01c4e19e301fc5ea3730
Dennis van der Veeke <MrDienns@dyescape.com>
2019-10-01 14:31:24 +0200

    added new integration testing mechanism and a way of testing the document id fetching function

--- pkg/migrate/migrator_integration_test.go
+++ pkg/migrate/migrator_integration_test.go
@@ -25,3 +25,36 @@ func TestTimeoutConnectionToDatabase(t *testing.T) {
 	err := migr.Connect()
 	assert.Error(t, err)
 }
+
+func TestCollectionNameDiscovery(t *testing.T) {
+	migr := migrator(t)
+	exp := []string{"a", "b", "c", "d", "e"}
+	act, err := migr.DocumentIds("_integration_test_collection")
+	assert.NoError(t, err)
+	assert.Equal(t, exp, act)
+	reset(migr, "_integration_test_collection")
+}
+
+func migrator(t *testing.T) *Migrator {
+	migr := NewMigrator("localhost:27017", "", "", "_integration_test_database")
+	err := migr.Connect()
+	assert.NoError(t, err)
+	coll := migr.client.Database(migr.database).Collection("_integration_test_collection")
+	coll.InsertMany(migr.ctx, mockDocuments())
+	return migr
+}
+
+func mockDocuments() []interface{} {
+	res := make([]interface{}, 5)
+	docIds := []string{"a", "b", "c", "d", "e"}
+	for i, id := range docIds {
+		m := make(map[string]interface{})
+		m["_id"] = id
+		res[i] = m
+	}
+	return res
+}
+
+func reset(m *Migrator, coll string) {
+	m.client.Database(m.database).Collection(coll).Drop(m.ctx)
+}
